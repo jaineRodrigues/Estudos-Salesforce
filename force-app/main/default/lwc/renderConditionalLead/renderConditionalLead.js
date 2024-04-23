3b1b85f280b3c2dbf9a56ae6db1d10f19b4efb0c
jaineRodrigues <jainer469@gmail.com>
2024-04-23 19:30:04 -0300

    add question 2 an updae question 1 for lead

--- force-app/main/default/lwc/renderConditionalLead/renderConditionalLead.js
+++ force-app/main/default/lwc/renderConditionalLead/renderConditionalLead.js
@@ -1,9 +1,11 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, api} from 'lwc';
 
 export default class LeadConditionalRendering extends LightningElement {
+    @api recordId;
     @track areDetailsVisible = false;
-
+    
   handleChange(event) {
+    const showDetailLead= event.detail.id;
     this.areDetailsVisible = event.target.checked;
   }
 }
