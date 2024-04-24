a6fe92310d030596a3047974a01b10a377a76b40
jaineRodrigues <jainer469@gmail.com>
2024-04-24 20:53:15 -0300

    LeadForEach done

--- force-app/main/default/lwc/leadForEach/leadForEach.js
+++ force-app/main/default/lwc/leadForEach/leadForEach.js
@@ -2,7 +2,6 @@ import { LightningElement, wire } from 'lwc';
 import getLeadsWithSource from '@salesforce/apex/leadForEachController.getLeadsWithSource';
 
 export default class LeadForEach extends LightningElement {
-    @wire(getLeadsWithSource)
     wiredLeads;
 
     @wire(getLeadsWithSource)
@@ -10,30 +9,7 @@ export default class LeadForEach extends LightningElement {
         if (data) {
             this.wiredLeads = data;
         } else if (error) {
-      
             console.error('Erro ao carregar leads:', error);
         }
     }
-
-    // leads = [
-    //     {
-    //         Id: '003171931112854375',
-    //         Name: 'Analy Taylor',
-    //         Email: 'ane@me.com',
-    //         Company: 'DBC'
-    //     },
-        
-    //     {
-    //         Id: '003848991274589432',
-    //         Name: 'Jennifer Wu',
-    //         Email: 'ane@me.com',
-    //         Company: 'DBC'
-    //     },
-    //     {
-    //         Id: '003848991274589432',
-    //         Name: 'Jennifer Wu',
-    //         Email: 'ane@me.com',
-    //         Company: 'DBC'
-    //     }
-    // ];
 }
